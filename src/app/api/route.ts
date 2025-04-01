93c7c4db8e7ecd087b483a57f6f5a02c4709834d
Cael <MR-Addict@qq.com>
2025-04-01 11:00:51 +0800

    feat: add demo

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,11 +2,20 @@ import { puppeteerPromise } from "@/lib/puppeteer/puppeteerPromise";
 
 export const maxDuration = 60;
 
-export async function GET() {
-  const browser = await puppeteerPromise;
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const title = await page.title();
-  await page.close();
-  return new Response(title);
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const searchParams = url.searchParams;
+  const site = searchParams.get("url");
+  if (!site) return new Response("No site provided", { status: 400 });
+
+  try {
+    const browser = await puppeteerPromise;
+    const page = await browser.newPage();
+    await page.goto(site);
+    const title = await page.title();
+    await page.close();
+    return new Response(title);
+  } catch (err) {
+    return new Response(null, { status: 500 });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import style from "./page.module.css";
+
 export default function Home() {
+  const [title, setTitle] = useState<{ success: boolean; message: string | number } | null>(null);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const url = formData.get("url") as string;
+    const res = await fetch(`/api?url=${url}`);
+    if (!res.ok) return setTitle({ success: false, message: res.status });
+
+    const title = await res.text();
+    setTitle({ success: true, message: title });
+  }
+
   return (
-    <main>
-      <h1>Next puppeteer scraper!</h1>
+    <main className={style.wrapper}>
+      <div className={style.container}>
+        <form onSubmit={handleSubmit}>
+          <input type="url" name="url" placeholder="Enter a URL" required />
+          <button type="submit">Submit</button>
+        </form>
+
+        {title && <p data-success={title.success}>{title.message}</p>}
+      </div>
     </main>
   );
 }
